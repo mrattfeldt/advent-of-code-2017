64fd88edc409a85900be7c44ee3c132274b01327
Magnus Rattfeldt <mrattfeldt@gmail.com>
2017-12-15 15:48:09 +0100

    Cleanup

--- day15/part1/solution.ml
+++ day15/part1/solution.ml
@@ -4,23 +4,18 @@
 (* ./solution.native *)
 
 let rec count ack a b n =
-  let matches a b =
-    let mask = (1 lsl 16) - 1 in
-    a land mask = b land mask
-  in
+  let matches a b = a land 0xffff = b land 0xffff in
   let next factor x =  (x * factor) mod 2147483647 in
   let (nexta, nextb) = (next 16807, next 48271) in
-  match n with
-    0  -> ack
-  | n  ->
-     let (na, nb) = (nexta a, nextb b) in
-     count (if matches na nb then succ ack else ack) na nb (pred n)
-                
-let start_a = 116
-let start_b = 299
+  if n = 0 then ack
+  else
+    let na, nb = nexta a, nextb b in
+    count (if matches na nb then succ ack else ack) na nb (pred n)
+
+let starta, startb = 116, 299
 
 let _ =
   if not !Sys.interactive then
-    count 0 start_a start_b 40000000
+    count 0 starta startb 40000000
     |> string_of_int
     |> print_endline
